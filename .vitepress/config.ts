7d81fe8ddb0f06fd3e1e2678580aa5ea32735e1e
Pavlo Tarasiuk <pasha.tarasyuk@gmail.com>
2025-12-11 16:52:09 +0200

    Change base path in VitePress

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vitepress';
 
 export default defineConfig({
-  base: '/',
+  base: '/docs/',
   title: 'DTLogix Docs',
   description: 'Документація DTLogix',
   lang: 'uk-UA',
